3a775aec49770423ee080e9d2730a86822025eb7
user <peach3139@naver.com>
2023-06-07 23:58:55 +0900

    사진 찍어 이미지 올리기

--- app/src/main/java/com/example/mygreenc/MainActivity2.kt
+++ app/src/main/java/com/example/mygreenc/MainActivity2.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.widget.Button
 
+import android.widget.Toast
 import android.widget.ImageView
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
@@ -13,11 +14,32 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
 
+    lateinit var bitmap: Bitmap
+    lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        imageView = findViewById(R.id.imageView2)
+        val btn = findViewById<Button>(R.id.btn2)
+
+        btn.setOnClickListener {
+            val intent : Intent = Intent(MidiaStore.ACTION_IMAGE_CAPTURE)
+            activitResult.launch(intent)
+        }
+
     }
+    private val activitResult: ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        if (result.resultCode == RESULT_OK) {
+            bitmap = result.data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(bitmap)
+        } else {
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
 }
 
