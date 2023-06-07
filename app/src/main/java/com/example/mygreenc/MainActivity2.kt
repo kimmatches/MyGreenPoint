55a3629e02c76175607f834c00bc5a9349e8a3a4
user <peach3139@naver.com>
2023-06-07 23:27:58 +0900

    사진 찍어 이미지 올리기

--- app/src/main/java/com/example/mygreenc/MainActivity2.kt
+++ app/src/main/java/com/example/mygreenc/MainActivity2.kt
@@ -1,11 +1,23 @@
 package com.example.mygreenc
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.widget.Button
+
+import android.widget.ImageView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
     }
 }
+
--- app/src/main/java/com/example/mygreenc/first.kt
+++ app/src/main/java/com/example/mygreenc/first.kt
@@ -1,13 +1,19 @@
 package com.example.mygreenc
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import androidx.appcompat.app.AppCompatActivity
 
 class first : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
-        Handler().postDelayed({ setContentView(R.layout.activity_main) }, 5000)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
