f2f311d9222a71623d37d4c2867a96b0756da797
user <peach3139@naver.com>
2023-06-08 20:17:06 +0900

    최종 완성본

--- app/src/main/java/com/example/mygreenc/Mypage.kt
+++ app/src/main/java/com/example/mygreenc/Mypage.kt
@@ -2,8 +2,11 @@ package com.example.mygreenc
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,6 +16,27 @@ class Mypage : AppCompatActivity() {
         setContentView(R.layout.activity_mypage)
 
         val Home = findViewById<ImageButton>(R.id.Home)
+        val uni = findViewById<Button>(R.id.uni)
+        val save = findViewById<Button>(R.id.save)
+        val count = findViewById<TextView>(R.id.count)
+        var counter = 8600
+
+        uni.setOnClickListener {
+            //토글 메세지 띄우기
+            Toast.makeText(this, " 1000원 기부하셨습니다.", Toast.LENGTH_SHORT).show()
+            counter-=1000
+
+            count.text = counter.toString()
+
+        }
+        save.setOnClickListener {
+
+            Toast.makeText(this, " 1000원 기부하셨습니다.", Toast.LENGTH_SHORT).show()
+            counter-=1000
+            count.text = counter.toString()
+
+        }
+
 
         Home.setOnClickListener{
             val intent = Intent(this,MainActivity::class.java)
